5f64c13de8fa00053545d9656789454919b10969
Iñigo Romero <45758515+InigoRomero@users.noreply.github.com>
2021-11-21 18:45:37 +0100

    add vector test

---
+++ tests/vector.cpp
@@ -0,0 +1,14 @@
+#include "../src/Vector.hpp"
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sys/time.h>
+#include <cstring> // for strcmp
+#include <stddef.h>
+#include <stdlib.h> // for system()
+
+int main()
+{
+	ft::vector<int>		wap;
+	std::vector<int>	stl;
+}
