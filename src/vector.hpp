623144269cdc7fd76ee4508188dd3d95a33268d8
InigoRomero <igoromero@gmail.com>
2021-12-12 18:23:17 +0100

    add vector insert single and fill

--- src/vector.hpp
+++ src/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: iromero- <iromero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 16:02:46 by iromero-          #+#    #+#             */
-/*   Updated: 2021/12/11 20:13:09 by iromero-         ###   ########.fr       */
+/*   Updated: 2021/12/12 18:22:12 by iromero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@
 
 template <typename iterator> class ft_iterator;
 
-#include <vector>
 namespace ft {
     template < typename T, typename Alloc = std::allocator<T> > 
     class vector {
@@ -32,8 +31,8 @@ namespace ft {
             typedef typename allocator_type::const_reference            const_reference;
             typedef typename allocator_type::pointer                    pointer;
             typedef typename allocator_type::const_pointer              const_pointer;
-            typedef ft_iterator<pointer>                            iterator;
-            typedef ft_iterator<const_pointer>                      const_iterator;
+            typedef ft_iterator<pointer>                                iterator;
+            typedef ft_iterator<const_pointer>                          const_iterator;
             typedef ReverseIterator<iterator>                           reverse_iterator;
             typedef ReverseIterator<const_iterator>                     const_reverse_iterator;
             typedef ptrdiff_t                                           difference_type;
@@ -54,11 +53,23 @@ namespace ft {
             ft_buff(NULL), ft_capacity(0) { static_cast<void>(alloc); }
             //fill 
             explicit vector (size_type n, const value_type& val = value_type(),
-                            const allocator_type& alloc = allocator_type());
+                            const allocator_type& alloc = allocator_type()):
+                            ft_buff(NULL), ft_capacity(n){
+                                ft_buff = ft_allocator.allocate(ft_capacity);
+                                size_t i = 0;
+                                while (i < ft_capacity)
+                                    ft_buff[i++] = val;
+                                static_cast<void>(alloc);
+                            }
             //range 
             template <class InputIterator>
                     vector (InputIterator first, InputIterator last,
-                            const allocator_type& alloc = allocator_type());
+                            const allocator_type& alloc = allocator_type()):
+                            ft_buff(NULL), ft_capacity(0){
+                                iterator position = begin();
+                                this->insert(position, first, last);
+                                static_cast<void>(alloc);
+                            };
             //copy	
             vector (const vector& x);
 
@@ -73,6 +84,7 @@ namespace ft {
                 return iterator(ft_buff + ft_capacity);
             }
             const_iterator  end() const {
+                std::cout << ft_capacity << "\n";
                 return const_iterator(ft_buff + ft_capacity);
             }
             reverse_iterator rbegin() {
@@ -94,7 +106,8 @@ namespace ft {
             size_type max_size() const { return ft_allocator.max_size(); }
 
             void resize (size_type n, value_type val = value_type()) { //later
-
+                (void)n;
+                (void)val;
             }
 
             size_type capacity() const { return ft_capacity; }
@@ -123,27 +136,71 @@ namespace ft {
             }
 
             /* *** MODIFIERS *** */
-            
             //Insert single lady
+            iterator insert (iterator position, const value_type& val) {
+                ft_capacity++;
+                if (ft_capacity > this->max_size())
+                        throw (std::length_error("vector::insert (fill)"));
+                pointer tmp = ft_allocator.allocate(ft_capacity);
+                iterator it = begin();
+                size_t i  = 0;
+                while (it < position)
+                    tmp[i++] = *it++;
+                iterator ret = iterator(tmp + i);
+                tmp[i++] = val;
+                while (i < ft_capacity)
+                    tmp[i++] = *it++;
+                ft_allocator.deallocate(ft_buff, ft_capacity);
+                ft_buff = tmp;
+                return ret;
+            }
+
+            //Insert multiple FILL
             void insert (iterator position, size_type n, const value_type& val) {
                 size_t aux = ft_capacity;
                 if (n > this->max_size())
                         throw (std::length_error("vector::insert (fill)"));
-                ft_capacity = (ft_capacity + n > ft_capacity) ? ft_capacity + n : ft_capacity;
+                ft_capacity += n;
                 pointer tmp = ft_allocator.allocate(ft_capacity);
                 iterator it = begin();
-                iterator ite = end();
-                int i  = 0;
+                size_t i  = 0;
                 while (it < position)
                     tmp[i++] = *it++;
-                for (size_t i = 0; i < n; i++)
-                    tmp[i++] = val;
-                while (it < ite)
+                n += i;
+                for (; i < n; i++)
+                    tmp[i] = val;
+                while (i < ft_capacity)
                     tmp[i++] = *it++;
                 ft_allocator.deallocate(ft_buff, aux);
                 ft_buff = tmp;
+            }
 
-        }
+            //Insert range
+            void insert (iterator position, iterator first, iterator last) {
+                size_t aux = ft_capacity;
+                size_t size = (last - first);
+                if (size > this->max_size())
+                    throw (std::length_error("vector::insert (fill)"));
+                ft_capacity = (ft_capacity + size > ft_capacity) ? ft_capacity + size : ft_capacity;
+                pointer tmp = ft_allocator.allocate(ft_capacity);
+                iterator it = begin();
+                size_t i  = 0;
+                while (it < position)
+                    tmp[i++] = *it++;
+                size += i;
+                while (i < size)
+                    tmp[i++] = *it++;
+                while (i < ft_capacity)
+                    tmp[i++] = *it++;
+                ft_allocator.deallocate(ft_buff, aux);
+                ft_buff = tmp;
+            };
+
+            //clear all content of the vector
+            void clear() {
+                ft_allocator.destroy(ft_buff);
+                ft_capacity = 0;
+            }
     };
     /* *** RELATIONAL OPERATORS *** */
 
@@ -193,55 +250,55 @@ public:
 
 private:
 
-        iterator_type                                                           m_current;
+        iterator_type                                                           ft_current;
 
 public:
 
-        ft_iterator(): m_current() {}
-        explicit ft_iterator(iterator_type x): m_current(x) {}
-        ft_iterator(const ft_iterator& x): m_current(x.m_current) {}
+        ft_iterator(): ft_current() {}
+        explicit ft_iterator(iterator_type x): ft_current(x) {}
+        ft_iterator(const ft_iterator& x): ft_current(x.ft_current) {}
         template <typename iter>
-        ft_iterator(const ft_iterator<iter>& i): m_current(i.base()) {}
+        ft_iterator(const ft_iterator<iter>& i): ft_current(i.base()) {}
         const iterator_type&    base() const {
-            return m_current;
+            return ft_current;
         }
 
         reference   operator*() const { 
-            return *m_current;
+            return *ft_current;
         }
         pointer     operator->() const {
-            return m_current;
+            return ft_current;
         }
         ft_iterator&  operator++() {
-            ++m_current;
+            ++ft_current;
             return *this;
         }
         ft_iterator    operator++(int) {
-            return ft_iterator(m_current++);
+            return ft_iterator(ft_current++);
         }
         ft_iterator&  operator--() {
-            --m_current;
+            --ft_current;
             return *this;
         }
         ft_iterator   operator--(int) {
-            return ft_iterator(m_current--);
+            return ft_iterator(ft_current--);
         }
         reference    operator[](difference_type n) const {
-            return m_current[n];
+            return ft_current[n];
         }
         ft_iterator&    operator+=(difference_type n) {
-            m_current += n;
+            ft_current += n;
             return *this;
         }
         ft_iterator     operator+(difference_type n) {
-            return ft_iterator(m_current + n);
+            return ft_iterator(ft_current + n);
         }
         ft_iterator&    operator-=(difference_type n) {
-            m_current -= n;
+            ft_current -= n;
             return *this;
         }
         ft_iterator   operator-(difference_type n) const {
-            return ft_iterator(m_current - n);
+            return ft_iterator(ft_current - n);
         } 
 };
 
